4c2a24ec90c8de84e3795d95ddc4fa8de216c3e4
kwak kun <kwakkun2002@gmail.com>
2025-09-21 20:31:18 +0900

    initalize에 데이터셋 다운로드 코드

--- initalize.py
+++ initalize.py
@@ -1,23 +1,38 @@
 # %%
 from datasets import load_dataset
 
+# %%
 # Parquet 브랜치에서 바로 불러오기
 ds = load_dataset(
     "nlphuji/flickr30k",
-    revision="refs/convert/parquet",   # ← 중요
+    revision="refs/convert/parquet",  # ← 중요
 )  # DatasetDict 반환 예상
 print(ds)  # train/val/test 등 스플릿 확인
+
+
+# %%
+# 이 코드로는 데이터셋을 불러올 수 없다.
+dataset = load_dataset("MRAMG/MRAMG-Bench", split="train", trust_remote_code=True)
+print(dataset)
+
+# 이런식으로 해야함
+# 이미지 파일이 너무 커저 git lfs(large file storage) 설치 필요
+# sudo apt update
+# sudo apt install git-lfs
+# git lfs install
+# git lfs install
+# git clone https://huggingface.co/datasets/MRAMG/MRAMG-Bench
+# cd MRAMG-Bench
 
 # %%
 ds["train"][0]
 
 # %%
-from transformers import CLIPProcessor, CLIPModel
 # %%
 import torch
+from transformers import CLIPModel, CLIPProcessor
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model_id = "openai/clip-vit-large-patch14"  # 768-d 공용 투영
 model = CLIPModel.from_pretrained(model_id).to(device).eval()
-proc  = CLIPProcessor.from_pretrained(model_id)
-
-
+proc = CLIPProcessor.from_pretrained(model_id)
